95e91abe19d95f89eb5412e3b407988c24bd67f3
whatsup123321 <deniodema@gmail.com>
2026-04-14 20:42:08 -0400

    final.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -4,19 +4,37 @@ import java.util.Collections;
 public class SudokuBoard {
     public static int[][] board = new int[9][9];
     public static void main(String[] args) {
+        createBoard();
+        displayBoard();
+    }
+    
+    public static void createBoard() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 board[i][j] = 0;
             }
         }
-        createBoard();
-        for (int[] s: board)
-        System.out.println(Arrays.toString(s));
-    }
-    public static void createBoard() {
         fillDiagonalFirst(0,0);
         fillCell(0, 0);
     }
+
+    public static void displayBoard() {
+        System.out.println("\nHello, Here is a solved Sudoku Puzzle: ");
+        for (int k = 0; k <=6; k+=3) {
+            System.out.println("+-------+-------+-------+");
+            for (int i = 0+k; i < 3 + k; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    if (j % 3 == 0){
+                        System.out.print("| ");
+                    }
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println("|");
+            }
+        }
+        System.out.println("+-------+-------+-------+");
+    }
+
     public static void fillDiagonalFirst(int r, int c) { //This method is done to take care of the 27 cells in the top-right, center, and bottom-right regions that are independent of solving and can be done first.
         ArrayList<Integer> arr = new ArrayList<Integer> (generateRandomArray());
         int startR = r;
@@ -60,6 +78,7 @@ public class SudokuBoard {
         board[r][c] = 0;
         return false;
     }
+
     public static boolean check3x3(int row, int col) {
         int refRow = row - row%3;
         int refCol = col - col%3;
@@ -72,6 +91,7 @@ public class SudokuBoard {
             return false;
         return true;
     }
+    
     public static ArrayList<Integer> generateRandomArray() { //Creates a list of numbers 1-9 randomized
         ArrayList<Integer> arr = new ArrayList<Integer>();
         for (int i = 1; i <= 9; i++) {
